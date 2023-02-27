6096a05b4aad06766affca963fa0c6878b9e2e86
guto <joaoaugustolanjoni@gmail.com>
2023-02-26 21:01:39 -0300

    First upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family:'Lucida Console', monospace;
+            background-color: black;
+        } 
+
+        h1, h3{
+            text-align: center;
+            color: green;
+        }
+        
+        a {
+            text-align: center;
+            color: purple;
+        }
+    </style>
+</head>
+<body>
+    <h1>more unix</h1>
+    <h3>still under construction</h3>
+
+    <h3>Follow us clicking <a href="https://twitter.com/moreunix">here</a>.</h3>
+</body>
+</html>
